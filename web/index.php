e0f4c5193d27f59627d749e6448aec326e84717d
Damien Walsh <me@damow.net>
2014-01-27 00:18:22 +0000

    Simplified config & htaccess.

--- web/index.php
+++ web/index.php
@@ -1,23 +1,5 @@
 <?php
-
 $loader = require_once '../vendor/autoload.php';
-
-class SomeClass
-{
-    public function __construct(SomeArgClass $foo)
-    {
-        print 'I was passed a ' . get_class($foo);
-    }
-}
-
-class SomeArgClass
-{
-    public function lol()
-    {
-        return 'LOL!';
-    }
-}
-
 $app = new Veto\App;
 $request = new \Veto\HTTP\Request;
 $request->initWithGlobals();
