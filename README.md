ecf09a0dde89c2c4fc86428b41fdb87314ba40a7
Damien Walsh <me@damow.net>
2014-01-27 22:04:17 +0000

    Sync skeleton code with Veto code.
     * Added a second demo route that displays the developer's name.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A basic skeleton installation for the Veto framework. If you want to start a new
 
 ### Installing
 
-1. **Clone this repository:**  
+1. **Clone this repository** and go into the new directory:  
 
         git clone https://github.com/themainframe/veto-skeleton.git veto
         cd veto
--- src/Controller/HelloWorld.php
+++ src/Controller/HelloWorld.php
@@ -12,11 +12,24 @@ class HelloWorld extends AbstractController
         $this->twig = $twig;
     }
 
-    public function sayHelloAction(Request $request)
+    public function sayHello(Request $request)
     {
         $resp = new Response($this->twig->render(
             'Welcome.html',
             array('version' => $this->get('app')->version)
+        ));
+
+        return $resp;
+    }
+
+    public function sayYourName(Request $request, $yourName)
+    {
+        $resp = new Response($this->twig->render(
+            'Welcome.html',
+            array(
+                'version' => $this->get('app')->version,
+                'name' => $yourName
+            )
         ));
 
         return $resp;
