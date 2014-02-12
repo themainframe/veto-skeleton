3a3abbd5fe0558c290a0815a8e2f9c7884032967
Damien Walsh <me@damow.net>
2014-02-12 21:22:55 +0000

    Changed the configuration files to suit the new loading mechanism.

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
 $loader = require_once '../vendor/autoload.php';
-$app = new Veto\App;
+
+// Create the application
+$app = new Veto\App('../config/app.json');
+
+// Handle the incoming request
 $request = new \Veto\HTTP\Request;
 $request->initWithGlobals();
 $app->handle($request);
