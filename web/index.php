cc8d6d0fb0cf1ee26ae5e9e95542e823e6c13ec8
Damien Walsh <me@damow.net>
2015-03-22 22:06:28 +0000

    Update: Front controller to match new Request APIs

--- web/index.php
+++ web/index.php
@@ -4,9 +4,11 @@ $loader = require_once '../vendor/autoload.php';
 // Create the application
 $app = new Veto\App('../config/app.yml');
 
+// Create a request from the current environment
+$environment = new \Veto\Collection\Bag($_SERVER);
+$request = \Veto\HTTP\Request::createFromEnvironment($environment);
+
 // Handle the incoming request
-$request = new \Veto\HTTP\Request;
-$request->initWithGlobals();
 $response = $app->handle($request);
 
 // Send the output
